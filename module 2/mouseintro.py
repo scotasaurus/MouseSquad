43e2db002ffdba39a7f91804ea1594059c35f1d5
Scot McIntosh <scot@outlook.com>
2015-12-01 00:04:53 -0800

    Added module 2 sample file
    Basic class to demonstrate importing into python.

---
+++ module 2/mouseintro.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+class MouseIntro:
+    def printHelloWorld(self):
+        print 'Hello World'
+    def printSum(self, value1, value2):
+        print value1 + value2
+    def multiplyValues(self, value1, value2):
+        return value1 * value2
+
+
